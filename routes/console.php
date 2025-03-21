d23474ae1e44e943d1e36810401aaffb5798cce5
Kreing44280 <kreingkoza44280@gmail.com>
2025-03-21 05:15:54 +0000

    run cronjob to console.php file

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Console\Kernel;
+use App\Models\News;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    app(Kernel::class)->dashboard();
+    app(Kernel::class)->sumNewsContent();
+    app(Kernel::class)->newsCount();
+    app(Kernel::class)->videoDuration();   
+})->everyMinute();
